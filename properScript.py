90c8f46ad467efb11a822686c629bf1285bea469
bluesentinelsec <31821088+bluesentinelsec@users.noreply.github.com>
2018-01-07 22:30:33 -0500

    Create properScript.py

---
+++ properScript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python -tt
+# This is a comment
+"""
+Doc string
+"""
+import sys
+def main():
+    "A 'Docstring' for the main function here"
+    print("You passed the argument " + sys.argv[1])
+
+if __name__=="__main__":
+    main()
